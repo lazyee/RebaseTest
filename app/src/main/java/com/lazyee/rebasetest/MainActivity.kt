e99b1a350ee573118153cc843f9bbe91ad70a2c9
Leeorz <378229364@qq.com>
2026-07-22 11:28:55 +0800

    [merge1] 第2次提交

--- app/src/main/java/com/lazyee/rebasetest/MainActivity.kt
+++ app/src/main/java/com/lazyee/rebasetest/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! mother fucker1",
+        text = "Hello $name! mother fucker2",
         modifier = modifier
     )
 }
