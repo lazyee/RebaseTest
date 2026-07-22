5a15ecaa5d6be63d42d9b4959bd11adee5754353
Leeorz <378229364@qq.com>
2026-07-22 17:22:30 +0800

    [master] 提交++++++++

--- app/src/main/java/com/lazyee/rebasetest/MainActivity.kt
+++ app/src/main/java/com/lazyee/rebasetest/MainActivity.kt
@@ -126,17 +126,6 @@ fun MasterWidget47(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Composable
-fun MasterWidget49999(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name! [merge1] Master Widget",
-        modifier = modifier
-    )
-}
-
-
-
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
