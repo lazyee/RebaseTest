b8011a7da840a67603eedda17853d07e73e30106
Leeorz <378229364@qq.com>
2026-07-22 17:16:51 +0800

    [master] 提交-------

--- app/src/main/java/com/lazyee/rebasetest/MainActivity.kt
+++ app/src/main/java/com/lazyee/rebasetest/MainActivity.kt
@@ -141,13 +141,6 @@ fun MasterWidget49999(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Composable
-fun MasterWidget49123(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name! [merge1] Master Widget",
-        modifier = modifier
-    )
-}
 
 
 
